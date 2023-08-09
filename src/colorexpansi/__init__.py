55628093ceff48a4ab35028bae4a7235c188cf8d
Brian Schubert <brianm.schubert@gmail.com>
2023-08-09 11:36:11 -0400

    Add exports for main formatters

--- src/colorexpansi/__init__.py
+++ src/colorexpansi/__init__.py
@@ -1,4 +1,9 @@
 import importlib.metadata
 
+from ._format import ANSIStringFormatter
+from ._logging import ANSILogFormatter
+
 # Package version, PEP-440 compatible.
 __version__ = importlib.metadata.version("colorexpansi")
+
+__all__ = ["ANSILogFormatter", "ANSIStringFormatter"]
