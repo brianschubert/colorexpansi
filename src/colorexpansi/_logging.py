286256b83601f93d1cd3bc92d9d4611b2ccfcaf2
Brian Schubert <brianm.schubert@gmail.com>
2023-08-09 11:17:45 -0400

    Add log message formatting

--- src/colorexpansi/_logging.py
+++ src/colorexpansi/_logging.py
@@ -6,9 +6,13 @@ from __future__ import annotations
 import logging
 from typing import Literal
 
+from ._format import ANSIStringFormatter
+
 
 class ANSILogFormatter(logging.Formatter):
     """Format ``logging.LogRecord``s using ANSI escape sequences."""
+
+    _formatter: ANSIStringFormatter
 
     def __init__(
         self,
@@ -16,5 +20,10 @@ class ANSILogFormatter(logging.Formatter):
         datefmt: str | None = None,
         style: Literal["%", "$", "{"] = "%",
         validate: bool = True,
+        formatter: ANSIStringFormatter = ANSIStringFormatter(),
     ) -> None:
         super().__init__(fmt, datefmt, style, validate)
+        self._formatter = formatter
+
+    def formatMessage(self, record: logging.LogRecord) -> str:
+        return self._formatter.format(self._fmt, record.__dict__)
