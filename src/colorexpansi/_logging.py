a46a75af8216fda4ebfdf7704a78f5917e668cb4
Brian Schubert <brianm.schubert@gmail.com>
2023-08-09 11:32:52 -0400

    Add note on why "{" style is needed

--- src/colorexpansi/_logging.py
+++ src/colorexpansi/_logging.py
@@ -19,6 +19,8 @@ class ANSILogFormatter(logging.Formatter):
         datefmt: str | None = None,
         formatter: ANSIStringFormatter = ANSIStringFormatter(),
     ) -> None:
+        # Must set style="{" so that timestamp detection in format string works correctly.
+        # Otherwise, asctime won't be added to the log records.
         super().__init__(fmt, datefmt, style="{", validate=False)
         self._formatter = formatter
 
